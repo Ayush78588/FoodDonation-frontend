1e9c167846e9f32c9d7b8dac15df46bbe069331a
Ayush Kumar <ayushkumar78588@gmail.com>
2025-06-08 14:20:28 +0530

    error solved

--- src/App.js
+++ src/App.js
@@ -48,7 +48,7 @@ export default function App() {
                     </div>
                   }
                 />
-              ) : (
+              ) : ( 
                 <Route
                   path="/user/:handle?"
                   element={
